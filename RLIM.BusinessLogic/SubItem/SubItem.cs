e55956b7d70fa4d2e58b57ee79ee589a61345b18
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-12-01 16:00:07 +0100

    Update: - SubItem geeft alleen nog de ID  van certificate, color en mainitem. - In SubItemController wordt certificate en color opgehaald.

--- RLIM.BusinessLogic/SubItem/SubItem.cs
+++ RLIM.BusinessLogic/SubItem/SubItem.cs
@@ -7,33 +7,15 @@ namespace RLIM.BusinessLogic
     {
         public int ID { get; private set; }
         public int MainItemID { get; private set; }
-
         public int CertificateID { get; private set; }
-        public CertificateDTO CertifcateDTO { get; private set; }
-
         public int ColorID { get; private set; }
-        public ColorDTO ColorDTO { get; private set; }
 
         public SubItem(SubItemDTO subItemDTO)
         {
             ID = subItemDTO.ID;
             MainItemID = subItemDTO.MainItemID;
-
             CertificateID = subItemDTO.CertificateID;
-            CertifcateDTO = GetCertificateDTO(CertificateID);
-
             ColorID = subItemDTO.ColorID;
-            ColorDTO = ColorFactoryDAL.GetCollectionDAL().Get(ColorID);
-        }
-
-        private CertificateDTO GetCertificateDTO(int id)
-        {
-            if (id != 0)
-            {
-                return CertificateFactoryDAL.GetCollectionDAL().Get(id);
-            }
-
-            return new CertificateDTO { ID = 0, Name = "None", Tier = 0 };
         }
 
         public void Update(int mainItemID, int certificateID, int colorID)
--- RLIM.UI/Controllers/SubItem/SubItemController.cs
+++ RLIM.UI/Controllers/SubItem/SubItemController.cs
@@ -7,9 +7,28 @@ namespace RLIM.UI.Controllers
 {
     public class SubItemController : Controller
     {
-        public IActionResult Index()
+        private CertificateModel GetCertificate(int id)
         {
-            return View(GetSubItems());
+            Certificate certificate = new CertificateCollection().Get(id);
+
+            return new CertificateModel
+            {
+                ID = certificate.ID,
+                Name = certificate.Name,
+                Tier = certificate.Tier
+            };
+        }
+
+        private ColorModel GetColor(int id)
+        {
+            Color color = new ColorCollection().Get(id);
+
+            return new ColorModel
+            {
+                ID = color.ID,
+                Name = color.Name,
+                Hex = color.Hex
+            };
         }
 
         private List<SubItemModel> GetSubItems()
@@ -18,30 +37,21 @@ namespace RLIM.UI.Controllers
 
             foreach (SubItem subItem in new SubItemCollection().GetAll())
             {
-                CertificateModel certificateModel = new CertificateModel
-                {
-                    ID = subItem.CertifcateDTO.ID,
-                    Name = subItem.CertifcateDTO.Name,
-                    Tier = subItem.CertifcateDTO.Tier
-                };
-
-                ColorModel colorModel = new ColorModel
-                {
-                    ID = subItem.ColorDTO.ID,
-                    Name = subItem.ColorDTO.Name,
-                    Hex = subItem.ColorDTO.Hex
-                };
-
                 subItemModels.Add(new SubItemModel
                 {
                     ID = subItem.ID,
                     MainItemID = subItem.MainItemID,
-                    Certificate = certificateModel,
-                    Color = colorModel
+                    Certificate = GetCertificate(subItem.CertificateID),
+                    Color = GetColor(subItem.ColorID)
                 });
             }
 
             return subItemModels;
+        }
+
+        public IActionResult Index()
+        {
+            return View(GetSubItems());
         }
     }
 }

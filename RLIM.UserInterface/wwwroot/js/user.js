f49de48cac205f68aabac9c150bbafa83767cc64
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-01-04 10:22:02 +0100

    Update: Als gebruiker op een andere Main-Item klikt, dan worden de Sub-Items van de vorige Main-Item onzichtbaar.

---
+++ RLIM.UserInterface/wwwroot/js/user.js
@@ -0,0 +1,43 @@
+﻿function ShowSubItems(e) {
+    let mainItemsContainer = document.getElementById("MainItemsContainer");
+    let mainItemContainer = e;
+    let subItemContainer = mainItemContainer.children.SubItems;
+
+    CloseAllMainItemContainers(mainItemContainer.id);
+
+    if (subItemContainer.classList.contains("hide")) {
+        mainItemContainer.classList.add("sub-items-visible");
+        mainItemsContainer.style.flexDirection = "column";
+
+        mainItemContainer.scrollIntoView({ block: "center", inline: "center" });
+
+        setTimeout(() => {
+            subItemContainer.classList.remove("hide");
+            subItemContainer.classList.add("show");
+            mainItemsContainer.style.flexDirection = "row";
+
+            mainItemContainer.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+        }, 500);
+    } else {
+        subItemContainer.classList.remove("show");
+        subItemContainer.classList.add("hide");
+        mainItemsContainer.style.flexDirection = "column";
+        mainItemContainer.classList.remove("sub-items-visible");
+
+        setTimeout(() => {
+            mainItemsContainer.style.flexDirection = "row";
+        }, 500);
+    }
+}
+
+function CloseAllMainItemContainers(mainItemContainerID) {
+    let mainItemContainers = document.getElementsByClassName("main-item");
+
+    for (var i = 0; i < mainItemContainers.length; i++) {
+        if (mainItemContainers[i].id != mainItemContainerID) {
+            mainItemContainers[i].classList.remove("sub-items-visible");
+            mainItemContainers[i].children.SubItems.classList.remove("show");
+            mainItemContainers[i].children.SubItems.classList.add("hide");
+        }
+    }
+}
